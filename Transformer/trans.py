2c0fffd54e2bf2abc1ad2759bbbf8609529e1a3b
unknown <1792364710@qq.com>
2021-07-29 21:21:54 +0800

    add trans.py

--- Transformer/trans.py
+++ Transformer/trans.py
@@ -336,5 +336,462 @@ dropout = 0.2
 x = mha_result
 ff = PositionwiseFeedForward(d_model, d_ff, dropout)
 ff_result = ff(x)
-print(ff_result)
-print(ff_result.shape)
+# print(ff_result)
+# print(ff_result.shape)
+
+
+# 2.5 规范化层
+# 通过LayerNorm实现规范化层的类
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        """初始化函数有两个参数, 一个是features, 表示词嵌入的维度,
+           另一个是eps它是一个足够小的数, 在规范化公式的分母中出现,
+           防止分母为0.默认是1e-6."""
+        super(LayerNorm, self).__init__()
+
+        # 根据features的形状初始化两个参数张量a2，和b2，第一个初始化为1张量，
+        # 也就是里面的元素都是1，第二个初始化为0张量，也就是里面的元素都是0，这两个张量就是规范化层的参数，
+        # 因为直接对上一层得到的结果做规范化公式计算，将改变结果的正常表征，因此就需要有参数作为调节因子，
+        # 使其即能满足规范化要求，又能不改变针对目标的表征.最后使用nn.parameter封装，代表他们是模型的参数。
+        self.a2 = nn.Parameter(torch.ones(features))
+        self.b2 = nn.Parameter(torch.zeros(features))
+
+        # 把eps传到类中
+        self.eps = eps
+
+    def forward(self, x):
+        """输入参数x代表来自上一层的输出"""
+        # 在函数中，首先对输入变量x求其最后一个维度的均值，并保持输出维度与输入维度一致.
+        # 接着再求最后一个维度的标准差，然后就是根据规范化公式，用x减去均值除以标准差获得规范化的结果，
+        # 最后对结果乘以我们的缩放参数，即a2，*号代表同型点乘，即对应位置进行乘法操作，加上位移参数b2.返回即可.
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a2 * (x - mean) / (std + self.eps) + self.b2
+
+features = d_model = 512
+eps = 1e-6
+
+# 输入x来自前馈全连接层的输出
+x = ff_result
+
+ln = LayerNorm(features, eps)
+ln_result = ln(x)
+# print(ln_result)
+# print(ln_result.shape)
+
+
+# 2.6 子层连接结构
+# 使用SublayerConnection来实现子层连接结构的类
+class SublayerConnection(nn.Module):
+    def __init__(self, size, dropout=0.1):
+        """它输入参数有两个, size以及dropout， size一般是都是词嵌入维度的大小， 
+           dropout本身是对模型结构中的节点数进行随机抑制的比率， 
+           又因为节点被抑制等效就是该节点的输出都是0，因此也可以把dropout看作是对输出矩阵的随机置0的比率.
+        """
+        super(SublayerConnection, self).__init__()
+        # 实例化了规范化对象self.norm
+        self.norm = LayerNorm(size)
+        # 又使用nn中预定义的droupout实例化一个self.dropout对象.
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x, sublayer):
+        """前向逻辑函数中, 接收上一个层或者子层的输入作为第一个参数，
+           将该子层连接中的子层函数作为第二个参数"""
+
+        # 我们首先对输出进行规范化，然后将结果传给子层处理，之后再对子层进行dropout操作，
+        # 随机停止一些网络中神经元的作用，来防止过拟合. 最后还有一个add操作， 
+        # 因为存在跳跃连接，所以是将输入x与dropout后的子层输出结果相加作为最终的子层连接输出.
+        return x + self.dropout(sublayer(self.norm(x)))
+
+size = 512
+dropout = 0.2
+head = 8
+d_model = 512
+
+# 令x为位置编码器的输出
+x = pe_result
+mask = Variable(torch.zeros(8, 4, 4))
+
+# 假设子层中装的是多头注意力层, 实例化这个类
+self_attn =  MultiHeadedAttention(head, d_model)
+
+# 使用lambda获得一个函数类型的子层
+sublayer = lambda x: self_attn(x, x, x, mask)
+
+sc = SublayerConnection(size, dropout)
+sc_result = sc(x, sublayer)
+# print(sc_result)
+# print(sc_result.shape)
+
+# 2.7 编码器层
+# 使用EncoderLayer类实现编码器层
+class EncoderLayer(nn.Module):
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        """它的初始化函数参数有四个，分别是size，其实就是我们词嵌入维度的大小，它也将作为我们编码器层的大小, 
+           第二个self_attn，之后我们将传入多头自注意力子层实例化对象, 并且是自注意力机制, 
+           第三个是feed_froward, 之后我们将传入前馈全连接层实例化对象, 最后一个是置0比率dropout."""
+        super(EncoderLayer, self).__init__()
+
+        # 首先将self_attn和feed_forward传入其中.
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+
+        # 如图所示, 编码器层中有两个子层连接结构, 所以使用clones函数进行克隆
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        # 把size传入其中
+        self.size = size
+
+    def forward(self, x, mask):
+        """forward函数中有两个输入参数，x和mask，分别代表上一层的输出，和掩码张量mask."""
+        # 里面就是按照结构图左侧的流程. 首先通过第一个子层连接结构，其中包含多头自注意力子层，
+        # 然后通过第二个子层连接结构，其中包含前馈全连接子层. 最后返回结果.
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+size = 512
+head = 8
+d_model = 512
+d_ff = 64
+x = pe_result
+dropout = 0.2
+self_attn = MultiHeadedAttention(head, d_model)
+ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+mask = Variable(torch.zeros(8, 4, 4))
+
+el = EncoderLayer(size, self_attn, ff, dropout)
+el_result = el(x, mask)
+# print(el_result)
+# print(el_result.shape)
+
+# 2.8 编码器
+# 使用Encoder类来实现编码器
+class Encoder(nn.Module):
+    def __init__(self, layer, N):
+        """初始化函数的两个参数分别代表编码器层和编码器层的个数"""
+        super(Encoder, self).__init__()
+        # 首先使用clones函数克隆N个编码器层放在self.layers中
+        self.layers = clones(layer, N)
+        # 再初始化一个规范化层, 它将用在编码器的最后面.
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        """forward函数的输入和编码器层相同, x代表上一层的输出, mask代表掩码张量"""
+        # 首先就是对我们克隆的编码器层进行循环，每次都会得到一个新的x，
+        # 这个循环的过程，就相当于输出的x经过了N个编码器层的处理. 
+        # 最后再通过规范化层的对象self.norm进行处理，最后返回结果. 
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+# 第一个实例化参数layer, 它是一个编码器层的实例化对象, 因此需要传入编码器层的参数
+# 又因为编码器层中的子层是不共享的, 因此需要使用深度拷贝各个对象.
+size = 512
+head = 8
+d_model = 512
+d_ff = 64
+c = copy.deepcopy
+attn = MultiHeadedAttention(head, d_model)
+ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+dropout = 0.2
+layer = EncoderLayer(size, c(attn), c(ff), dropout)
+
+# 编码器中编码器层的个数N
+N = 8
+mask = Variable(torch.zeros(8, 4, 4))
+
+en = Encoder(layer, N)
+en_result = en(x, mask)
+# print(en_result)
+# print(en_result.shape)
+
+# 三、解码器部分实现
+
+# 3.1 解码器层
+# 使用DecoderLayer的类实现解码器层
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        """初始化函数的参数有5个, 分别是size，代表词嵌入的维度大小, 同时也代表解码器层的尺寸，
+            第二个是self_attn，多头自注意力对象，也就是说这个注意力机制需要Q=K=V， 
+            第三个是src_attn，多头注意力对象，这里Q!=K=V， 第四个是前馈全连接层对象，最后就是droupout置0比率.
+        """
+        super(DecoderLayer, self).__init__()
+        # 在初始化函数中， 主要就是将这些输入传到类中
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        # 按照结构图使用clones函数克隆三个子层连接对象.
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, source_mask, target_mask):
+        """forward函数中的参数有4个，分别是来自上一层的输入x，
+           来自编码器层的语义存储变量mermory， 以及源数据掩码张量和目标数据掩码张量.
+        """
+        # 将memory表示成m方便之后使用
+        m = memory
+
+        # 将x传入第一个子层结构，第一个子层结构的输入分别是x和self-attn函数，因为是自注意力机制，所以Q,K,V都是x，
+        # 最后一个参数是目标数据掩码张量，这时要对目标数据进行遮掩，因为此时模型可能还没有生成任何目标数据，
+        # 比如在解码器准备生成第一个字符或词汇时，我们其实已经传入了第一个字符以便计算损失，
+        # 但是我们不希望在生成第一个字符时模型能利用这个信息，因此我们会将其遮掩，同样生成第二个字符或词汇时，
+        # 模型只能使用第一个字符或词汇信息，第二个字符以及之后的信息都不允许被模型使用.
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, target_mask))
+
+        # 接着进入第二个子层，这个子层中常规的注意力机制，q是输入x; k，v是编码层输出memory， 
+        # 同样也传入source_mask，但是进行源数据遮掩的原因并非是抑制信息泄漏，而是遮蔽掉对结果没有意义的字符而产生的注意力值，
+        # 以此提升模型效果和训练速度. 这样就完成了第二个子层的处理.
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, source_mask))
+
+        # 最后一个子层就是前馈全连接子层，经过它的处理后就可以返回结果.这就是我们的解码器层结构.
+        return self.sublayer[2](x, self.feed_forward)
+
+# 类的实例化参数与解码器层类似, 相比多出了src_attn, 但是和self_attn是同一个类.
+head = 8
+size = 512
+d_model = 512
+d_ff = 64
+dropout = 0.2
+self_attn = src_attn = MultiHeadedAttention(head, d_model, dropout)
+
+# 前馈全连接层也和之前相同 
+ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+
+# x是来自目标数据的词嵌入表示, 但形式和源数据的词嵌入表示相同, 这里使用per充当.
+x = pe_result
+
+# memory是来自编码器的输出
+memory = en_result
+
+# 实际中source_mask和target_mask并不相同, 这里为了方便计算使他们都为mask
+mask = Variable(torch.zeros(8, 4, 4))
+source_mask = target_mask = mask
+
+dl = DecoderLayer(size, self_attn, src_attn, ff, dropout)
+dl_result = dl(x, memory, source_mask, target_mask)
+# print(dl_result)
+# print(dl_result.shape)
+
+# 3.2 解码器
+# 使用类Decoder来实现解码器
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        """初始化函数的参数有两个，第一个就是解码器层layer，第二个是解码器层的个数N."""
+        super(Decoder, self).__init__()
+        # 首先使用clones方法克隆了N个layer，然后实例化了一个规范化层. 
+        # 因为数据走过了所有的解码器层后最后要做规范化处理. 
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, source_mask, target_mask):
+        """forward函数中的参数有4个，x代表目标数据的嵌入表示，memory是编码器层的输出，
+           source_mask, target_mask代表源数据和目标数据的掩码张量"""
+
+        # 然后就是对每个层进行循环，当然这个循环就是变量x通过每一个层的处理，
+        # 得出最后的结果，再进行一次规范化返回即可. 
+        for layer in self.layers:
+            x = layer(x, memory, source_mask, target_mask)
+        return self.norm(x)
+
+# 分别是解码器层layer和解码器层的个数N
+size = 512
+d_model = 512
+head = 8
+d_ff = 64
+dropout = 0.2
+c = copy.deepcopy
+attn = MultiHeadedAttention(head, d_model)
+ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+layer = DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout)
+N = 8
+
+# 输入参数与解码器层的输入参数相同
+x = pe_result
+memory = en_result
+mask = Variable(torch.zeros(8, 4, 4))
+source_mask = target_mask = mask
+
+de = Decoder(layer, N)
+de_result = de(x, memory, source_mask, target_mask)
+# print(de_result)
+# print(de_result.shape)
+
+# 四、输出部分实现
+# nn.functional工具包装载了网络层中那些只进行计算, 而没有参数的层
+import torch.nn.functional as F
+
+# 将线性层和softmax计算层一起实现, 因为二者的共同目标是生成最后的结构
+# 因此把类的名字叫做Generator, 生成器类
+class Generator(nn.Module):
+    def __init__(self, d_model, vocab_size):
+        """初始化函数的输入参数有两个, d_model代表词嵌入维度, vocab_size代表词表大小."""
+        super(Generator, self).__init__()
+        # 首先就是使用nn中的预定义线性层进行实例化, 得到一个对象self.project等待使用, 
+        # 这个线性层的参数有两个, 就是初始化函数传进来的两个参数: d_model, vocab_size
+        self.project = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        """前向逻辑函数中输入是上一层的输出张量x"""
+        # 在函数中, 首先使用上一步得到的self.project对x进行线性变化, 
+        # 然后使用F中已经实现的log_softmax进行的softmax处理.
+        # 在这里之所以使用log_softmax是因为和我们这个pytorch版本的损失函数实现有关, 在其他版本中将修复.
+        # log_softmax就是对softmax的结果又取了对数, 因为对数函数是单调递增函数, 
+        # 因此对最终我们取最大的概率值没有影响. 最后返回结果即可.
+        return F.log_softmax(self.project(x), dim=-1)
+
+# 词嵌入维度是512维
+d_model = 512
+
+# 词表大小是1000
+vocab_size = 1000
+
+# 输入x是上一层网络的输出, 我们使用来自解码器层的输出
+x = de_result
+
+gen = Generator(d_model, vocab_size)
+gen_result = gen(x)
+# print(gen_result)
+# print(gen_result.shape)
+
+# 五、模型构建
+# 5.1 编码器-解码器结构的代码实现
+# 使用EncoderDecoder类来实现编码器-解码器结构
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, source_embed, target_embed, generator):
+        """初始化函数中有5个参数, 分别是编码器对象, 解码器对象, 
+           源数据嵌入函数, 目标数据嵌入函数,  以及输出部分的类别生成器对象
+        """
+        super(EncoderDecoder, self).__init__()
+        # 将参数传入到类中
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = source_embed
+        self.tgt_embed = target_embed
+        self.generator = generator
+
+    def forward(self, source, target, source_mask, target_mask):
+        """在forward函数中，有四个参数, source代表源数据, target代表目标数据, 
+           source_mask和target_mask代表对应的掩码张量"""
+
+        # 在函数中, 将source, source_mask传入编码函数, 得到结果后,
+        # 与source_mask，target，和target_mask一同传给解码函数.
+        return self.generator(self.decode(self.encode(source, source_mask), source_mask,
+                            target, target_mask))
+
+    def encode(self, source, source_mask):
+        """编码函数, 以source和source_mask为参数"""
+        # 使用src_embed对source做处理, 然后和source_mask一起传给self.encoder
+        return self.encoder(self.src_embed(source), source_mask)
+
+    def decode(self, memory, source_mask, target, target_mask):
+        """解码函数, 以memory即编码器的输出, source_mask, target, target_mask为参数"""
+        # 使用tgt_embed对target做处理, 然后和source_mask, target_mask, memory一起传给self.decoder
+        return self.decoder(self.tgt_embed(target), memory, source_mask, target_mask)
+
+vocab_size = 1000
+d_model = 512
+encoder = en
+decoder = de
+source_embed = nn.Embedding(vocab_size, d_model)
+target_embed = nn.Embedding(vocab_size, d_model)
+generator = gen
+
+# 假设源数据与目标数据相同, 实际中并不相同
+source = target = Variable(torch.LongTensor([[100, 2, 421, 508], [491, 998, 1, 221]]))
+
+# 假设src_mask与tgt_mask相同，实际中并不相同
+source_mask = target_mask = Variable(torch.zeros(8, 4, 4))
+
+ed = EncoderDecoder(encoder, decoder, source_embed, target_embed, generator)
+ed_result = ed(source, target, source_mask, target_mask)
+# print(ed_result)
+# print(ed_result.shape)
+
+# 5.2 Tansformer模型构建过程的代码分析
+def make_model(source_vocab, target_vocab, N=6, 
+               d_model=512, d_ff=2048, head=8, dropout=0.1):
+    """该函数用来构建模型, 有7个参数，分别是源数据特征(词汇)总数，目标数据特征(词汇)总数，
+       编码器和解码器堆叠数，词向量映射维度，前馈全连接网络中变换矩阵的维度，
+       多头注意力结构中的多头数，以及置零比率dropout."""
+
+    # 首先得到一个深度拷贝命令，接下来很多结构都需要进行深度拷贝，
+    # 来保证他们彼此之间相互独立，不受干扰.
+    c = copy.deepcopy
+
+    # 实例化了多头注意力类，得到对象attn
+    attn = MultiHeadedAttention(head, d_model)
+
+    # 然后实例化前馈全连接类，得到对象ff 
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+
+    # 实例化位置编码类，得到对象position
+    position = PositionalEncoding(d_model, dropout)
+
+    # 根据结构图, 最外层是EncoderDecoder，在EncoderDecoder中，
+    # 分别是编码器层，解码器层，源数据Embedding层和位置编码组成的有序结构，
+    # 目标数据Embedding层和位置编码组成的有序结构，以及类别生成器层. 
+    # 在编码器层中有attention子层以及前馈全连接子层，
+    # 在解码器层中有两个attention子层以及前馈全连接层.
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), 
+                             c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, source_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, target_vocab), c(position)),
+        Generator(d_model, target_vocab))
+
+    # 模型结构完成后，接下来就是初始化模型中的参数，比如线性层中的变换矩阵
+    # 这里一但判断参数的维度大于1，则会将其初始化成一个服从均匀分布的矩阵，
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+    return model
+
+source_vocab = 11
+target_vocab = 11 
+N = 6
+# 其他参数都使用默认值 
+
+# if __name__ == '__main__':
+#     res = make_model(source_vocab, target_vocab, N)
+#     print(res)
+
+
+# 六、模型基本测试运行
+# 第一步: 构建数据集生成器
+# 导入工具包Batch, 它能够对原始样本数据生成对应批次的掩码张量
+from pyitcast.transformer_utils import Batch  
+
+def data_generator(V, batch, num_batch):
+    """该函数用于随机生成copy任务的数据, 它的三个输入参数是V: 随机生成数字的最大值+1, 
+       batch: 每次输送给模型更新一次参数的数据量, num_batch: 一共输送num_batch次完成一轮
+    """
+    # 使用for循环遍历nbatches
+    for i in range(num_batch):
+        # 在循环中使用np的random.randint方法随机生成[1, V)的整数, 
+        # 分布在(batch, 10)形状的矩阵中, 然后再把numpy形式转换称torch中的tensor.
+        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
+
+        # 接着使数据矩阵中的第一列数字都为1, 这一列也就成为了起始标志列, 
+        # 当解码器进行第一次解码的时候, 会使用起始标志列作为输入.
+        data[:, 0] = 1
+
+        # 因为是copy任务, 所有source与target是完全相同的, 且数据样本作用变量不需要求梯度
+        # 因此requires_grad设置为False
+        source = Variable(data, requires_grad=False)
+        target = Variable(data, requires_grad=False)
+
+        # 使用Batch对source和target进行对应批次的掩码张量生成, 最后使用yield返回
+        yield Batch(source, target) 
+
+# 将生成0-10的整数
+V = 11
+
+# 每次喂给模型20个数据进行参数更新
+batch = 20 
+
+# 连续喂30次完成全部数据的遍历, 也就是1轮
+num_batch = 30
+
+if __name__ == '__main__':
+    res = data_generator(V, batch, num_batch)
+    print(res)
