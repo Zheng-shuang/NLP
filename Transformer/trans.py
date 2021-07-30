cf1f47de92c4e1359c66a0ea421e928bdd090168
unknown <1792364710@qq.com>
2021-07-30 10:21:08 +0800

    add trans.py

--- Transformer/trans.py
+++ Transformer/trans.py
@@ -757,6 +757,7 @@ N = 6
 
 
 # 六、模型基本测试运行
+
 # 第一步: 构建数据集生成器
 # 导入工具包Batch, 它能够对原始样本数据生成对应批次的掩码张量
 from pyitcast.transformer_utils import Batch  
@@ -792,6 +793,81 @@ batch = 20
 # 连续喂30次完成全部数据的遍历, 也就是1轮
 num_batch = 30
 
+# if __name__ == '__main__':
+#     res = data_generator(V, batch, num_batch)
+#     print(res)
+
+
+# 第二步: 获得Transformer模型及其优化器和损失函数
+
+# 导入优化器工具包get_std_opt, 该工具用于获得标准的针对Transformer模型的优化器 
+# 该标准优化器基于Adam优化器, 使其对序列到序列的任务更有效.
+from pyitcast.transformer_utils import get_std_opt
+
+# 导入标签平滑工具包, 该工具用于标签平滑, 标签平滑的作用就是小幅度的改变原有标签值的值域
+# 因为在理论上即使是人工的标注数据也可能并非完全正确, 会受到一些外界因素的影响而产生一些微小的偏差
+# 因此使用标签平滑来弥补这种偏差, 减少模型对某一条规律的绝对认知, 以防止过拟合. 通过下面示例了解更多.
+from pyitcast.transformer_utils import LabelSmoothing
+
+# 导入损失计算工具包, 该工具能够使用标签平滑后的结果进行损失的计算, 
+# 损失的计算方法可以认为是交叉熵损失函数.
+from pyitcast.transformer_utils import SimpleLossCompute
+
+# 使用make_model获得model
+model = make_model(V, V, N=2)
+
+# 使用get_std_opt获得模型优化器
+model_optimizer = get_std_opt(model)
+
+# 使用LabelSmoothing获得标签平滑对象
+criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+
+# 使用SimpleLossCompute获得利用标签平滑结果的损失计算方法
+loss = SimpleLossCompute(model.generator, criterion, model_optimizer)
+# print(loss)
+
+# 第三步: 运行模型进行训练和评估
+# 导入模型单轮训练工具包run_epoch, 该工具将对模型使用给定的损失函数计算方法进行单轮参数更新.
+# 并打印每轮参数更新的损失结果.
+from pyitcast.transformer_utils import run_epoch
+
+# 第四步: 使用模型进行贪婪解码
+# 导入贪婪解码工具包greedy_decode, 该工具将对最终结进行贪婪解码
+# 贪婪解码的方式是每次预测都选择概率最大的结果作为输出, 
+# 它不一定能获得全局最优性, 但却拥有最高的执行效率.
+from pyitcast.transformer_utils import greedy_decode 
+
+def run(model, loss, epochs=10):
+    """模型训练函数, 共有三个参数, model代表将要进行训练的模型
+       loss代表使用的损失计算方法, epochs代表模型训练的轮数"""
+
+    # 遍历轮数
+    for epoch in range(epochs):
+        # 模型使用训练模式, 所有参数将被更新
+        model.train()
+        # 训练时, batch_size是20
+        run_epoch(data_generator(V, 8, 20), model, loss)
+
+        # 模型使用评估模式, 参数将不会变化 
+        model.eval()
+        # 评估时, batch_size是5
+        run_epoch(data_generator(V, 8, 5), model, loss)
+    
+    # 模型进入测试模式
+    model.eval()
+
+    # 假定的输入张量
+    source = Variable(torch.LongTensor([[1,3,2,5,4,6,7,8,9,10]]))
+
+    # 定义源数据掩码张量, 因为元素都是1, 在我们这里1代表不遮掩
+    # 因此相当于对源数据没有任何遮掩.
+    source_mask = Variable(torch.ones(1, 1, 10))
+
+    # 最后将model, src, src_mask, 解码的最大长度限制max_len, 默认为10
+    # 以及起始标志数字, 默认为1, 我们这里使用的也是1
+    result = greedy_decode(model, source, source_mask, max_len=10, start_symbol=1)
+    print(result)
+
+
 if __name__ == '__main__':
-    res = data_generator(V, batch, num_batch)
-    print(res)
+    run(model, loss) 
